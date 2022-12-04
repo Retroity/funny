bbe32db81eaa5d2718b0a93553da993517ba0e95
Retroity <Retroity@users.noreply.github.com>
2022-12-03 21:12:59 -0600

    I asked the bot to fix it's work again.

--- MAKE_funny.py
+++ MAKE_funny.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python
 
-import sys
+import random
 
 def make_funny(file_name):
     with open(file_name, 'r') as f:
         lines = f.readlines()
     
-    with open(file_name, 'w') as f:
-        for line in lines:
-            if line.strip() == sys.argv[1]:
-                f.write(line.replace(sys.argv[1], 'funny'))
-            else:
-                f.write(line)
+    print(random.choice(lines))
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        make_funny('funny.txt')
-    else:
-        print("Please provide a string to replace")
+    make_funny('funny.txt')
