71aeccc3fc7196fab612db8b2b7d636693142dad
Retroity <Retroity@users.noreply.github.com>
2022-12-03 20:56:14 -0600

    Create MAKE_funny.py

---
+++ MAKE_funny.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+
+def make_funny(file_name):
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    
+    with open(file_name, 'w') as f:
+        for line in lines:
+            if line.strip() == sys.argv[1]:
+                f.write(line.replace(sys.argv[1], 'funny'))
+            else:
+                f.write(line)
+
+if __name__ == '__main__':
+    make_funny('funny.txt')
